b850e1b5d420679dde4d7656c55c714c94604c2b
Alondra <riveragutierrez7b1@gmail.com>
2019-06-12 21:36:03 -0500

    Vercion 1

---
+++ app/src/main/java/com/example/controlbt/map.java
@@ -0,0 +1,20 @@
+package com.example.controlbt;
+
+public class map {
+    private Double lat;
+    private Double log;
+    public void setLat(Double lat){
+        this.lat=lat;
+    }
+
+    public Double getLat(){
+        return lat;
+    }
+    public void setLog(Double log){
+        this.log=log;
+    }
+
+    public Double getLog(){
+        return log;
+    }
+}
